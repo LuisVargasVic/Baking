d7c0281067402832a25707e73b2de827015de0bc
Luis Vargas <luisev96@gmail.com>
2019-11-15 09:29:37 -0600

    Test with own ViewAssertion

--- app/src/androidTest/java/com/udacity/luisev96/baking/MainActivityScreenTest.java
+++ app/src/androidTest/java/com/udacity/luisev96/baking/MainActivityScreenTest.java
@@ -1,7 +1,13 @@
 package com.udacity.luisev96.baking;
 
+import android.util.Log;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -15,16 +21,15 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.udacity.luisev96.baking.utils.Position.atPosition;
 
 // Annotation to specify AndroidJUnitRunner class as the default test runner
 @RunWith(AndroidJUnit4.class)
 public class MainActivityScreenTest {
 
+    private static final String TAG = MainActivityScreenTest.class.getSimpleName();
     private static final String BROWNIES = "Brownies";
     private IdlingResource mIdlingResource;
 
@@ -44,8 +49,22 @@ public class MainActivityScreenTest {
     // the ListActivity opens up with the correct recipe name displayed.
     @Test
     public void clickRecyclerViewItem_OpensListActivity() {
-        // Click item at position 3
-        onView(withId(R.id.rv_recipes)).check(matches(atPosition(1, hasDescendant(withText(BROWNIES))))).perform(click());
+        // Click item at position 1
+        onView(withId(R.id.rv_recipes)).check(
+                new ViewAssertion() {
+                    @Override
+                    public void check(View view, NoMatchingViewException noViewFoundException) {
+                        if (noViewFoundException != null) throw noViewFoundException;
+                        try {
+                            RecyclerView recyclerView = (RecyclerView) view;
+                            RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(1);
+                            hasDescendant(withText(BROWNIES)).matches(viewHolder.itemView);
+                        } catch (Exception e) {
+                            Log.wtf(TAG, e.getLocalizedMessage());
+                        }
+                    }
+                }).perform(click());
+
     }
 
     // Remember to unregister resources when not needed to avoid malfunction.
